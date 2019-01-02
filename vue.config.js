a561fb221c5c847ce115eacde007d884bf285acd
cklwblove <lwbhtml@163.com>
2019-01-02 14:31:48 +0800

    feat: 添加环境变量文件，增加webpack相关插件，vue.config.js更新，添加新特性

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
+'use strict'
+
 const path = require('path')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const resolve = (dir) => {
@@ -30,12 +33,57 @@ const genPlugins = () => {
     )
   }
 
+  // HtmlWebpackIncludeAssetsPlugin
+  // 为静态资源文件添加 hash，防止缓存
+  plugins.push(
+    new HtmlWebpackIncludeAssetsPlugin({
+      assets: ['config.local.js'],
+      append: false,
+      hash: true
+    })
+  )
+
   return plugins;
 }
 
 module.exports = {
+  /**
+   * You can set by yourself according to actual condition
+   * You will need to set this if you plan to deploy your site under a sub path,
+   * for example GitHub pages. If you plan to deploy your site to https://foo.github.io/bar/,
+   * then assetsPublicPath should be set to "/bar/".
+   * In most cases please use '/' !!!
+   * Detail https://cli.vuejs.org/config/#baseurl
+   */
   baseUrl: './',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  productionSourceMap: false,
+  // webpack-dev-server 相关配置
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 3000,
+    https: false,
+    hotOnly: false,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
+  // css相关配置
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: isProd() ? true : false,
+    // 开启 CSS source maps?
+    sourceMap: isProd() ? true : false,
+    // css预设器配置项
+    loaderOptions: {},
+    // 启用 CSS modules for all css / pre-processor files.
+    modules: false
+  },
   configureWebpack: () => ({
+    name: 'vue-cli3-template',
     resolve: {
       alias: {
         '@': resolve('src'),
@@ -79,6 +127,12 @@ module.exports = {
       .options({
         symbolId: 'icon-[name]'
       })
+      .end()
+
+    config
+      .when(process.env.NODE_ENV === 'development',
+        config => config.devtool('cheap-source-map')
+      )
 
     // plugin
     // webpack-html-plugin
@@ -100,48 +154,62 @@ module.exports = {
         return args
       })
 
+    // optimization
+    config
+      .when(process.env.NODE_ENV === 'production',
+        config => {
+          config
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .use('script-ext-html-webpack-plugin', [{
+              // `runtime` must same as runtimeChunk name. default is `runtime`
+              inline: /runtime\..*\.js$/
+            }])
+          config
+            .optimization.splitChunks({
+            chunks: 'all',
+            cacheGroups: {
+              libs: {
+                name: 'chunk-libs',
+                test: /[\\/]node_modules[\\/]/,
+                priority: 10,
+                chunks: 'initial' // 只打包初始时依赖的第三方
+              },
+              commons: {
+                name: 'chunk-commons',
+                test: resolve('src/components'), // 可自定义拓展你的规则
+                minChunks: 3, // 最小公用次数
+                priority: 5,
+                reuseExistingChunk: true
+              }
+            }
+          })
+          config.optimization.runtimeChunk('single')
+        }
+      )
+
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    if (process.env.npm_config_report) {
-      config
-        .plugin('webpack-bundle-analyzer')
-        .use(BundleAnalyzerPlugin, [{
-          analyzerPort: 8888,
-          generateStatsFile: false
-        }])
-    }
+    // `npm run analyz`
+    config
+      .when(process.env.IS_ANALYZ,
+        config => config
+          .plugin('webpack-bundle-analyzer')
+          .use(BundleAnalyzerPlugin, [{
+            analyzerPort: 8888,
+            generateStatsFile: false
+          }])
+      )
 
     // `npm run build --generate_report`
-    if (process.env.npm_config_generate_report) {
-      config
-        .plugin('webpack-bundle-analyzer')
-        .use(BundleAnalyzerPlugin, [{
-          analyzerMode: 'static',
-          reportFilename: 'bundle-report.html',
-          openAnalyzer: false
-        }])
-    }
-
-  },
-  // css相关配置
-  css: {
-    // 是否使用css分离插件 ExtractTextPlugin
-    extract: isProd() ? true : false,
-    // 开启 CSS source maps?
-    sourceMap: isProd() ? true : false,
-    // css预设器配置项
-    loaderOptions: {
-    },
-    // 启用 CSS modules for all css / pre-processor files.
-    modules: false
-  },
-  // webpack-dev-server 相关配置
-  devServer: {
-    open: process.platform === 'darwin',
-    host: '0.0.0.0',
-    port: 3000,
-    https: false,
-    hotOnly: false
+    config
+      .when(process.env.npm_config_generate_report,
+        config => config
+          .plugin('webpack-bundle-analyzer-report')
+          .use(BundleAnalyzerPlugin, [{
+            analyzerMode: 'static',
+            reportFilename: 'bundle-report.html',
+            openAnalyzer: false
+          }])
+      )
   }
 };
