58a83df222b9ab97ecfba66a6925794bd4649e15
cklwblove <lwbhtml@163.com>
2019-01-17 17:52:08 +0800

    refactor: baseUrl -> publicPath

--- vue.config.js
+++ vue.config.js
@@ -55,7 +55,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail https://cli.vuejs.org/config/#baseurl
    */
-  baseUrl: './',
+  publicPath: './',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV !== 'production',
   productionSourceMap: false,
