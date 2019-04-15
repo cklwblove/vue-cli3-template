d5157c0f48838f2235cb6a493fd90e1d684e1ffa
cklwblove <lwbhtml@163.com>
2019-04-15 15:24:48 +0800

    feat: 增加 mixins(派发与广播—— dispatch 和 broadcast 方法)

---
+++ src/mixins/emitter.js
@@ -0,0 +1,79 @@
+/**
+ * 派发与广播—— dispatch 和 broadcast 方法
+ * 父子组件（含跨级）间的通信问题
+ *
+ * dispatch 向上级派发事件，只要是它的父级（一级或多级以上），都可以在组件内通过 `$on` （或 events，2.x 已废弃）监听到
+ * broadcast 是由上级向下级广播事件的。
+ *
+ * 在子组件调用 dispatch 方法，向上级指定的组件实例（最近的）上触发自定义事件，并传递数据，且该上级组件已预先通过 `$on` 监听了这个事件；
+ * 相反，在父组件调用 broadcast 方法，向下级指定的组件实例（最近的）上触发自定义事件，并传递数据，且该下级组件已预先通过 `$on` 监听了这个事件。
+ */
+
+function broadcast(componentName, eventName, params) {
+  this.$children.forEach((child) => {
+    const name = child.$options.name;
+
+    if (name === componentName) {
+      child.$emit.apply(child, [eventName].concat(params));
+    } else {
+      broadcast.apply(child, [componentName, eventName].concat([params]));
+    }
+  });
+}
+
+export default {
+  methods: {
+    dispatch(componentName, eventName, params) {
+      let parent = this.$parent || this.$root;
+      let name = parent.$options.name;
+
+      while (parent && (!name || name !== componentName)) {
+        parent = parent.$parent;
+
+        if (parent) {
+          name = parent.$options.name;
+        }
+      }
+      if (parent) {
+        parent.$emit.apply(parent, [eventName].concat(params));
+      }
+    },
+    broadcast(componentName, eventName, params) {
+      broadcast.call(this, componentName, eventName, params);
+    }
+  }
+};
+
+/*
+* eg:有 **A.vue** 和 **B.vue** 两个组件，其中 B 是 A 的子组件，中间可能跨多级，在 A 中向 B 通信：
+* <!-- A.vue -->
+<template>
+	<button @click="handleClick">触发事件</button>
+</template>
+<script>
+  import Emitter from '../mixins/emitter.js';
+
+  export default {
+    name: 'componentA',
+    mixins: [ Emitter ],
+    methods: {
+      handleClick () {
+        this.broadcast('componentB', 'on-message', 'Hello Vue.js');
+      }
+    }
+  }
+</script>
+
+// B.vue
+export default {
+  name: 'componentB',
+  created () {
+    this.$on('on-message', this.showMessage);
+  },
+  methods: {
+    showMessage (text) {
+      window.alert(text);
+    }
+  }
+}
+* */
