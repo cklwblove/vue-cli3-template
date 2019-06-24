4a846c70822d05d78193810913d505ab3fe76040
cklwblove <lwbhtml@163.com>
2019-06-24 16:34:42 +0800

    runjs renamed to tasksfile

--- build/index.js
+++ build/index.js
@@ -1,22 +1,28 @@
 'use strict';
 
-const { run } = require('runjs')
-const chalk = require('chalk')
-const rawArgv = process.argv.slice(2)
-const args = rawArgv.join(' ')
+const { sh, cli } = require('tasksfile');
+const chalk = require('chalk');
+const rawArgv = process.argv.slice(2);
+const args = rawArgv.join(' ');
 
 // 便于捕捉 build 之后的错误，然后进行自定义处理
 // 配合 jenkins 执行 job
-run(`vue-cli-service build ${args}`, {
-  async: true,
-  stdio: 'inherit'
-}).then((output) => {
-  console.log(chalk.cyan(output || ''));
-}).catch((err) => {
-  console.error('\n');
-  console.error(chalk.magenta('编译打包出错了 ~~~~(>_<)~~~~ \n'))
-  console.error(chalk.magenta('具体错误信息如下 \n'))
-  console.error(chalk.red(`${err}.\n`))
-  console.log(chalk.red('  Build failed with errors.\n'))
-  process.exit(1)
-})
+function command() {
+  sh(`vue-cli-service build ${args}`, {
+    async: true,
+    stdio: 'inherit',
+  })
+    .then((output) => {
+      console.log(chalk.cyan(output || ''));
+    })
+    .catch((err) => {
+      console.error('\n');
+      console.error(chalk.magenta('编译打包出错了 ~~~~(>_<)~~~~ \n'));
+      console.error(chalk.magenta('具体错误信息如下 \n'));
+      console.error(chalk.red(`${err}.\n`));
+      console.log(chalk.red('  Build failed with errors.\n'));
+      process.exit(1);
+    });
+}
+
+command();
