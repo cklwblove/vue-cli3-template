07aba4a92ed0448f8a80d1dcb75eb88b7b43fca2
cklwblove <lwbhtml@163.com>
2019-09-06 17:48:26 +0800

    完善打包的压缩包名称

--- build/zip.js
+++ build/zip.js
@@ -2,20 +2,28 @@
 
 const path = require('path');
 const fs = require('fs');
+const pack = require('../package');
 const archiver = require('archiver');
-const {formatDate} = require('@liwb/cloud-utils');
+const { formatDate } = require('@liwb/cloud-utils');
 
 // create a file to stream archive data to.
-var output = fs.createWriteStream(`${path.resolve(__dirname, '../')}/dist_${formatDate(new Date(), 'yyyy-MM-dd_HH:mm:ss')}.zip`);
+var output = fs.createWriteStream(
+  `${path.resolve(__dirname, '../')}/${pack.name}-${pack.version}-${formatDate(
+    new Date(),
+    'yyyy-MM-dd_HH:mm:ss'
+  )}.zip`
+);
 var archive = archiver('zip', {
-  zlib: { level: 9 } // Sets the compression level.
+  zlib: { level: 9 }, // Sets the compression level.
 });
 
 // listen for all archive data to be written
 // 'close' event is fired only when a file descriptor is involved
 output.on('close', function() {
   console.log(archive.pointer() + ' total bytes');
-  console.log('archiver has been finalized and the output file descriptor has closed.');
+  console.log(
+    'archiver has been finalized and the output file descriptor has closed.'
+  );
 });
 
 // This event is fired when the data source is drained no matter what was the data source.
