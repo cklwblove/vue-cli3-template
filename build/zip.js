39b75ddce37a4a51147c4b2c8f03610059ac9bec
cklwblove <lwbhtml@163.com>
2019-09-16 23:28:38 +0800

    优化 zip 命令及实现

--- build/zip.js
+++ build/zip.js
@@ -1,58 +1,57 @@
-'use strict';
+#!/usr/bin/env node
 
-const path = require('path');
 const fs = require('fs');
-const pack = require('../package');
+const path = require('path');
 const archiver = require('archiver');
 const { formatDate } = require('@liwb/cloud-utils');
 
-// create a file to stream archive data to.
-var output = fs.createWriteStream(
-  `${path.resolve(__dirname, '../')}/${pack.name}-${pack.version}-${formatDate(
+const DEST_DIR = path.join(__dirname, '../dist');
+const DEST_ZIP_DIR = path.join(__dirname, '../dist-zip');
+
+const extractExtensionData = () => {
+  const extPackageJson = require('../package.json');
+
+  return {
+    name: extPackageJson.name,
+    version: extPackageJson.version,
+  };
+};
+
+const makeDestZipDirIfNotExists = () => {
+  if (!fs.existsSync(DEST_ZIP_DIR)) {
+    fs.mkdirSync(DEST_ZIP_DIR);
+  }
+};
+
+const buildZip = (src, dist, zipFilename) => {
+  console.info(`Building ${zipFilename}...`);
+
+  const archive = archiver('zip', { zlib: { level: 9 } });
+  const stream = fs.createWriteStream(path.join(dist, zipFilename));
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(src, false)
+      .on('error', (err) => reject(err))
+      .pipe(stream);
+
+    stream.on('close', () => resolve());
+    archive.finalize();
+  });
+};
+
+const main = () => {
+  const { name, version } = extractExtensionData();
+  const zipFilename = `${name}-v${version}_${formatDate(
     new Date(),
     'yyyy-MM-dd_HH:mm:ss'
-  )}.zip`
-);
-var archive = archiver('zip', {
-  zlib: { level: 9 }, // Sets the compression level.
-});
+  )}.zip`;
 
-// listen for all archive data to be written
-// 'close' event is fired only when a file descriptor is involved
-output.on('close', function() {
-  console.log(archive.pointer() + ' total bytes');
-  console.log(
-    'archiver has been finalized and the output file descriptor has closed.'
-  );
-});
+  makeDestZipDirIfNotExists();
 
-// This event is fired when the data source is drained no matter what was the data source.
-// It is not part of this library but rather from the NodeJS Stream API.
-// @see: https://nodejs.org/api/stream.html#stream_event_end
-output.on('end', function() {
-  console.log('Data has been drained');
-});
+  buildZip(DEST_DIR, DEST_ZIP_DIR, zipFilename)
+    .then(() => console.info('OK'))
+    .catch(console.err);
+};
 
-// good practice to catch warnings (ie stat failures and other non-blocking errors)
-archive.on('warning', function(err) {
-  if (err.code === 'ENOENT') {
-    // log warning
-  } else {
-    // throw error
-    throw err;
-  }
-});
-
-// good practice to catch this error explicitly
-archive.on('error', function(err) {
-  throw err;
-});
-
-// pipe archive data to the file
-archive.pipe(output);
-
-archive.directory('./dist/', false);
-
-// finalize the archive (ie we are done appending files but streams have to finish yet)
-// 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-archive.finalize();
+main();
