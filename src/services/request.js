3b9502d43110eaeb129c456436902ce7f93b4426
cklwblove <lwbhtml@163.com>
2019-12-04 15:48:37 +0800

    refactor(code): 增加 axios responseLog

--- src/services/request.js
+++ src/services/request.js
@@ -46,6 +46,25 @@ const axiosConfig = {
   error: (error) => Promise.reject(error),
 };
 
+function responseLog(response) {
+  if (process.env.NODE_ENV === 'development') {
+    const randomColor = `rgba(${Math.round(Math.random() * 255)},${Math.round(
+      Math.random() * 255
+    )},${Math.round(Math.random() * 255)})`;
+    console.log(
+      '%c┍------------------------------------------------------------------┑',
+      `color:${randomColor};`
+    );
+    console.log('| 请求地址：', response.config.url);
+    console.log('| 请求参数：', Qs.parse(response.config.data));
+    console.log('| 返回数据：', response.data);
+    console.log(
+      '%c┕------------------------------------------------------------------┙',
+      `color:${randomColor};`
+    );
+  }
+}
+
 /**
  * 全局请求响应处理
  * 添加一个返回拦截器 （于transformResponse之后处理）
@@ -54,6 +73,7 @@ const axiosConfig = {
 const axiosResponse = {
   success: (response) => {
     window.$eventBus.$emit('isBrokenNetwork', false);
+    responseLog(response);
     return checkStatus(response);
   },
   error: (error) => {
