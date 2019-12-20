a7d2cc9844ea434378ccc0323a1d920834b4361b
cklwblove <lwbhtml@163.com>
2019-12-20 11:51:19 +0800

    feat: 以一个文件的方式使用解析好的 webpack 配置

---
+++ webstorm.config.js
@@ -0,0 +1,6 @@
+'use strict';
+// 以一个文件的方式使用解析好的配置
+// https://cli.vuejs.org/zh/guide/webpack.html#%E4%BB%A5%E4%B8%80%E4%B8%AA%E6%96%87%E4%BB%B6%E7%9A%84%E6%96%B9%E5%BC%8F%E4%BD%BF%E7%94%A8%E8%A7%A3%E6%9E%90%E5%A5%BD%E7%9A%84%E9%85%8D%E7%BD%AE
+
+const webpackConfig = require('@vue/cli-service/webpack.config.js');
+module.exports = webpackConfig;
